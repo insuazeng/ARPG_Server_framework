4fa2c6032c3bec6b3c2689bef8f7394ec2757216
insuazeng <insuazengxiaofeng@gmail.com>
2020-04-09 17:09:53 +0800

    network
    code

---
+++ network_protocol/UserProto/协议生成(服务端用).bat
@@ -0,0 +1,10 @@
+..\protoc.exe --descriptor_set_out=../../config/server/Config/ProtobufData/UserProtoPublic.pb ./UserProtoPublic.proto
+..\protoc.exe --descriptor_set_out=../../config/server/Config/ProtobufData/UserProto001.pb ./UserProto001.proto
+..\protoc.exe --descriptor_set_out=../../config/server/Config/ProtobufData/UserProto002.pb ./UserProto002.proto
+..\protoc.exe --descriptor_set_out=../../config/server/Config/ProtobufData/UserProto003.pb ./UserProto003.proto
+..\protoc.exe --descriptor_set_out=../../config/server/Config/ProtobufData/UserProto004.pb ./UserProto004.proto
+..\protoc.exe --descriptor_set_out=../../config/server/Config/ProtobufData/UserProto006.pb ./UserProto006.proto
+..\protoc.exe --descriptor_set_out=../../config/server/Config/ProtobufData/UserProto007.pb ./UserProto007.proto
+
+echo "done"
+@pause
